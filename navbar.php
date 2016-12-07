fec0f9d702c1f1e44b7bac5ed9056404017c6109
Mile Kovac <mile@Miles-MacBook-Pro.local>
2016-12-07 13:49:47 +0100

    php navbar

--- navbar.php
+++ navbar.php
@@ -1,4 +1,5 @@
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<?php
+echo '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
 <nav class="navbar navbar-default navbar-static-top">
   <div class="container">
@@ -33,3 +34,5 @@
     </div><!--/.nav-collapse -->
   </div>
 </nav>
+';
+?>
